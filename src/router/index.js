f19475ef7b04e0e1e9ae047f31fc326e5bd580b9
Evanora <admin@evanora.top>
2023-04-21 17:14:34 +0800

    frist

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import login from '../components/login.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+
+    {
+
+      path:'/login',
+      component :login
+    },
+    {
+
+      path:'/',component:login
+
+    }
+
+  ]
+})
+
+export default router
