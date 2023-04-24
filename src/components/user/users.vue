4b7cd9d37fd3832c0633e445f94930e6d2162ea7
Evanora <admin@evanora.top>
2023-04-24 22:14:17 +0800

    修正合并导致的文件内容丢失

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -6,31 +6,111 @@
 
     </el-breadcrumb>
 
-
+    <!-- 白板 -->
     <el-card class="box-card">
 
-        <div class="mt-4">
-            <el-input v-model="searchDate" placeholder="请输入" class="input-with-select">
-                <template #append>
-                    <el-button :icon="Search" />
-                </template>
-            </el-input>
-        </div>
-        
+
+
+
+        <!-- 布局 -->
+        <el-row :gutter="20">
+            <el-col :span="7">
+
+                <!-- 输入框 -->
+                <el-input v-model="searchDate" placeholder="请输入" class="input-with-select">
+                    <template #append>
+                        <el-button :icon="Search" />
+                    </template>
+                </el-input>
+
+            </el-col>
+
+            <!-- 添加用户 -->
+            <el-col :span="4">
+
+                <el-button type="primary">添加用户</el-button>
+
+            </el-col>
+
+        </el-row>
+
+
+        <el-table :data="userlist" stripe style="width: 100%" border>
+            <el-table-column type="index" label="#" width="50" />
+            <el-table-column prop="username" label="姓名" width="180" />
+            <el-table-column prop="email" label="邮箱" width="180" />
+            <el-table-column prop="mobile" label="电话" width="180" />
+            <el-table-column prop="role_name" label="角色" width="180" />
+            <el-table-column prop="mg_state" label="状态" width="180" >
+                
+            </el-table-column>
+            <el-table-column  label="操作" />
+        </el-table>
+
+
+
+
+
+
+
+
+
+
     </el-card>
 </template>
 
 
 <script >
 export default {
+    created() {
 
+        this.getUserList()
+
+
+    },
+    methods: {
+
+        async getUserList() {
+
+            const { data: res } = await this.$axios.get('users', { params: this.QueryInfo })
+            if (res.meta.status !== 200) {
+                ElMessage({ message: '获取列表失败，已强制退出！', type: 'warning', })
+
+                window.sessionStorage.clear()
+                this.$router.push("/login");
+                return
+
+            }
+            console.log(res);
+
+            this.userlist = res.data.users
+            this.total = res.data.total
+            console.log(this.userlist);
+            console.log(this.total);
+
+
+        }
+
+
+    },
 
 
     data() {
 
+
         return {
 
-            searchDate:''
+            searchDate: '',
+            QueryInfo: {
+
+                query: '',
+                pagenum: 1,
+                pagesize: 2
+
+            },
+            userlist: [],
+            total: 2
+
 
 
 
@@ -44,7 +124,7 @@ export default {
 
 
 import { Search } from '@element-plus/icons-vue'
-
+import { ElMessage } from 'element-plus'
 
 </script>
 
@@ -52,14 +132,21 @@ import { Search } from '@element-plus/icons-vue'
 
 <style scoped>
 
-.mt-4{
+.el-table{
+
+
+    margin-top: 20px;
+
+}
+
+/* .mt-4 {
 
     width: 300px;
-}
+} */
 
 
 .input-with-select .el-input-group__prepend {
-  background-color: var(--el-fill-color-blank);
+    background-color: var(--el-fill-color-blank);
 }
 
 
