270837337510433cb436740ba3aaa9fcb9c55a58
Evanora <admin@evanora.top>
2023-04-28 18:27:26 +0800

    完成了角色列表子权限的显示，以及角色名称的编辑

--- src/components/rights/roles.vue
+++ src/components/rights/roles.vue
@@ -15,7 +15,76 @@
 
 
         <el-table :data="rightslist" stripe style="width: 100%" border>
-            <el-table-column label="" width="70" />
+            <el-table-column type="expand" label="" width="70">
+                <template v-slot="scope">
+
+
+
+                    <div v-if="scope.row.children.length === 0">
+
+                        <el-tag>没有任何权限！</el-tag>
+                    </div>
+
+
+                    <el-row :class="['row_buttom']" v-for="(item1, il) in scope.row.children" :key="item1.id">
+
+                        <!-- 第一级 -->
+                        <el-col :span="3">
+
+                            <el-tag closable @close="deleteRightsForRoles(scope.row.id, item1.id)">{{ item1.authName
+                            }}</el-tag>
+                            <el-icon>
+                                <CaretRight />
+                            </el-icon>
+
+
+
+                        </el-col>
+
+
+                        <!-- 第二级 -->
+                        <el-col :span="21">
+                            <!-- <pre>{{ item1.children }}</pre> -->
+
+
+                            <el-row :class="['row_buttom']" v-for="(item2, i2) in item1.children" :key="item2.id">
+
+
+                                <!-- 第二级子一级 -->
+                                <el-col :span="3">
+
+                                    <el-tag closable type="success" @close="deleteRightsForRoles(scope.row.id, item2.id)">{{
+                                        item2.authName }}</el-tag>
+                                    <el-icon>
+                                        <CaretRight />
+                                    </el-icon>
+
+
+
+
+                                </el-col>
+
+                                <!-- 第二级子二级 -->
+                                <el-col :span="30" v-for="(item3, i3) in item2.children" :key="item3.id">
+
+                                    <!-- {{ scope.row.id}}
+                                    {{ item3.id}} -->
+                                    <el-tag closable type="warning" @close="deleteRightsForRoles(scope.row.id, item3.id)">{{
+                                        item3.authName }}</el-tag>
+
+
+
+                                </el-col>
+                            </el-row>
+                        </el-col>
+
+                    </el-row>
+
+
+
+
+                </template>
+            </el-table-column>
             <el-table-column type="index" label="#" width="70" />
             <el-table-column prop="roleName" label="角色名称" />
             <el-table-column prop="roleDesc" label="角色描述" />
@@ -25,7 +94,7 @@
                     <!-- {{scope.row.id }} -->
 
                     <el-button type="primary" :icon="Edit"
-                        @click="dialogVisible_changeUser = true, changeFormDataId = scope.row.id">编辑</el-button>
+                        @click="dialogVisible_changeRoles = true, dialogVisible_changeRoles_id = scope.row.id">编辑</el-button>
 
                     <!-- 删除 -->
                     <el-button type="danger" :icon="Delete" @click="deleteRoles(scope.row.id)">删除</el-button>
@@ -58,7 +127,7 @@
 
 
 
-    <!-- 弹出层-添加用户 （未设计）-->
+    <!-- 弹出层-添加角色-->
     <el-dialog v-model="dialogVisible_addRoles" width="30%" title="添加角色" @close="clearDialofForm_addRoles()">
 
 
@@ -79,6 +148,38 @@
                 <el-button @click="dialogVisible_addRoles = false">取消</el-button>
                 <!-- 触发提交 -->
                 <el-button type="primary" @click="addRolesDatePreChaeck()">
+                    提交
+                </el-button>
+            </span>
+        </template>
+    </el-dialog>
+
+
+    <!-- --------------------------------- -->
+
+
+
+    <!-- 弹出层-编辑角色 -->
+    <el-dialog v-model="dialogVisible_changeRoles" width="30%" title="编辑角色" @close="clearDialofForm_changeRoles()">
+
+
+        <el-form ref="changeRolesDateRef" :model="changeRolesDate" :rules="addFormData_addRolesDate" label-width="70px">
+            <el-form-item prop="roleName" label="名称">
+                <el-input v-model="changeRolesDate.roleName" />
+            </el-form-item>
+
+            <el-form-item prop="roleDesc" label="描述">
+                <el-input v-model="changeRolesDate.roleDesc" />
+            </el-form-item>
+
+
+        </el-form>
+
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible_changeRoles = false">取消</el-button>
+                <!-- 触发提交 -->
+                <el-button type="primary" @click="changeRolesDatePreChaeck()">
                     提交
                 </el-button>
             </span>
@@ -107,7 +208,14 @@ export default {
                 roleName: '',
                 roleDesc: ''
             },
+            changeRolesDate: {
+
+                roleName: '',
+                roleDesc: ''
+            },
             dialogVisible_addRoles: false,
+            dialogVisible_changeRoles: false,
+            dialogVisible_changeRoles_id: 0,
             addFormData_addRolesDate: {
 
 
@@ -145,6 +253,7 @@ export default {
 
             // console.log(res);
             this.rightslist = res.data
+            // console.log(res.data);
 
 
         },
@@ -170,6 +279,11 @@ export default {
 
             this.$refs.addRolesDateRef.resetFields()
 
+        },
+        clearDialofForm_changeRoles() {
+
+
+            this.$refs.changeRolesDateRef.resetFields()
         },
         addRolesDatePreChaeck() {
 
@@ -206,6 +320,65 @@ export default {
 
 
         },
+
+        // 删除角色指定权限
+        async deleteRightsForRoles(roleId, rightId) {
+
+            const { data: res } = await this.$axios.delete(`roles/${roleId}/rights/${rightId}`)
+            if (res.meta.status !== 200) {
+                ElMessage({ message: '取消权限失败', type: 'warning', })
+
+                return
+
+            }
+            this.getRightsList()
+
+        },
+
+        // 编辑角色信息
+        changeRolesDatePreChaeck() {
+
+            this.$refs.changeRolesDateRef.validate(valid => {
+
+                if (!valid) return
+
+
+                // console.log(valid);
+
+                this.dialogVisible_changeRoles = false
+                this.changeRolesDate_fun()
+
+            })
+
+
+
+
+        },
+
+        async changeRolesDate_fun() {
+
+
+
+            // 发起请求
+            const { data: res } = await this.$axios.put(`roles/${this.dialogVisible_changeRoles_id}`, this.changeRolesDate)
+            if (res.meta.status !== 200) {
+                ElMessage({ message: '编辑提交角色失败', type: 'warning', })
+
+                return
+
+            }
+            this.getRightsList()
+
+
+
+
+
+        }
+
+
+
+
+
     }
 }
 
@@ -218,7 +391,7 @@ export default {
 
 
 import { ElMessage } from 'element-plus'
-import { Delete, Edit, Search, Setting } from '@element-plus/icons-vue'
+import { Delete, Edit, Search, Setting, CaretRight } from '@element-plus/icons-vue'
 
 </script>
 
@@ -233,5 +406,21 @@ import { Delete, Edit, Search, Setting } from '@element-plus/icons-vue'
 .addroles {
 
     margin-bottom: 20px;
+}
+
+.el-tag {
+
+    margin: 7px;
+}
+
+
+.row_buttom {
+
+    border-bottom: 1px solid #eee;
+}
+
+.row_top {
+
+    border-bottom: 1px solid #eee;
 }
 </style>
